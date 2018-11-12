e97abd435241faa82228e604638fda485bb62d8f
NikhilGupta1997 <nikhilgupta1997@gmail.com>
2018-11-13 04:17:28 +0800

    Autocomplete added to search landmark

--- phpFiles/searchCarpark.php
+++ phpFiles/searchCarpark.php
@@ -1,50 +1,89 @@
-<html lang="en">
+<html lang = "en">
+   
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
-
     <title>SearchForCarpark</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap theme -->
-    <link href="../../dist/css/bootstrap-theme.min.css" rel="stylesheet">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="theme.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"/>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+      $( function() {
+        var availableTags = [
+          "Current Location",
+          "Administration Building",
+          "Chinese Heritage Centre",
+          "Community Herb Garden",
+          "Nanyang Arch",
+          "Nanyang Auditorium",
+          "Nanyang Executive Centre",
+          "Nanyang Lake",
+          "Nanyang Tablet",
+          "North Spine",
+          "The Hive",
+          "The Quad",
+          "Research Techno Plaza",
+          "Student Services Centre",
+          "South Spine",
+          "University Health Service Building",
+          "Yunnan Garden"
+        ];
+        $( "#tags" ).autocomplete({
+          source: availableTags
+        });
+      } );
+    </script>
   </head>
+
   <style type="text/css">
-  input[type=button], input[type=submit], input[type=reset] 
-  {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 16px 32px;
-    text-decoration: none;
-    margin: 4px 2px;
-    cursor: pointer;
-  }
+    input[type=button], input[type=submit], input[type=reset] {
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      padding: 16px 32px;
+      text-decoration: none;
+      margin: 4px 2px;
+      cursor: pointer;
+    }
+  </style>
+  <style>
+        h1 {
+            font-size: 20px;
+            color: #111;
+        }
+
+        .content {
+            width: 80%;
+            margin: 0 auto;
+            margin-top: 50px;
+        }
+
+        .tt-hint,
+        .city {
+            border: 2px solid #CCCCCC;
+            border-radius: 8px 8px 8px 8px;
+            font-size: 24px;
+            height: 45px;
+            line-height: 30px;
+            outline: medium none;
+            padding: 8px 12px;
+            width: 400px;
+        }
+
+        .tt-dropdown-menu {
+            width: 400px;
+            margin-top: 5px;
+            padding: 8px 12px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            border-radius: 8px 8px 8px 8px;
+            font-size: 18px;
+            color: #111;
+            background-color: #F1F1F1;
+        }
   </style>
   <script>
-
     function getLocation() {
+        window.alert ("Getting location");
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else { 
@@ -52,99 +91,71 @@
         }
     }
 
-    function showPosition(position) 
-    {
+    function showPosition(position) {
+      window.alert ("position location");
         document.myform.cLat.value = position.coords.latitude;
         document.myform.cLon.value = position.coords.longitude;
+        window.alert ("position going");
     }
+  </script>
 
-</script>
+  <?php
+    $db = pg_connect("host=nlpserver.postgres.database.azure.com port=5432 dbname=carpark_db user=nikhil@nlpserver password=MOMDAD<3nick");
 
-<body>
+    $cLat=$cLon="";
+    $dLat=$dLon="";
 
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    $destination_string="";
+    $vehicle_id="ayush_car";
+    $use_current_location="";
 
-$cLat=$cLon="";
-$dLat=$dLon="";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $use_current_location=$_POST["use_current_location"];
+      $cLat= $_POST["cLat"];
+      $cLon= $_POST["cLon"];
+      if(is_null($_POST[destination_string])) {
+        $dLat= $_POST["cLat"];
+        $dLon= $_POST["cLon"];
+      }
+      else {
+        $result= pg_query($db,"SELECT * FROM table_landmarks WHERE name = '$_POST[destination_string]'" );
+        $myrow = pg_fetch_assoc($result);
+        if(is_null($myrow[latitude]) || is_null($myrow[longitude])) {
+          $dLat=$_POST["cLat"];
+          $dLon=$_POST["cLon"];
+        } else {
+          $dLat=$myrow[latitude];
+          $dLon=$myrow[longitude];
+        }
+      }
+      
+      echo "<script type='text/javascript'>window.top.location='carparkResult.php?cLat=$cLat&cLon=$cLon&dLat=$dLat&dLon=$dLon&useCurrLoc=$use_current_location';</script>"; exit;
+    }
+    else
+    {
+      echo "<script> getLocation(); </script>";
+    }
+  ?>  
 
-$destination_string=$use_current_location="";
-$vehicle_id="ayush_car";
-$use_current_location="";
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-  $use_current_location=$_POST["use_current_location"];
-  $cLat= $_POST["cLat"];
-  $cLon= $_POST["cLon"];
-  $destination_string= $_POST["destination_string"];
-  if(strcmp($destination_string,"executive_centre")==0)
-  {
-    $dLat=0;
-    $dLon=1;
-  }
-  else if(strcmp($destination_string,"macdonalds")==0)
-  {
-    $dLat=2;
-    $dLon=3;
-  }
-  else if(strcmp($destination_string,"pioneer_hall")==0)
-  {
-    $dLat=4;
-    $dLon=5;
-  }
-  echo "<script type='text/javascript'>window.top.location='carparkResult.php?cLat=$cLat&cLon=$cLon&dLat=$dLat&dLon=$dLon&useCurrLoc=$use_current_location';</script>"; exit;
-  // header("Location: carparkResult.php?cLat=$cLat&cLon=$cLon&dLat=$dLat&dLon=$dLon&useCurrLoc=$use_current_location");
-  // die();  
+  <center>
+  <div class="content">
 
-  // $totalLink="carparkResult.php?cLat=".$cLat."&cLon=".$cLon."&dLat=".$dLat."&dLon=".$dLon."&useCurrLoc=".$use_current_location;
-  
+  <form name="myform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
+    Select Destination: 
+    <input id="tags" class="city" type="text" name="destination_string" placeholder="Current Location" value="<?php echo $destination_string;?>">
+    <br><br>
+    Use Current Location? : 
+    <select name="use_current_location" value="<?php echo $use_current_location;?>">
+    <option value="yes">YES</option>
+    <option value="no">NO</option>
+    </select>
+    <br><br>
+    <input type="hidden" name="cLat" value="<?php echo $cLat;?>" >
+    <input type="hidden" name="cLon" value="<?php echo $cLon;?>" >
+    <input type="submit" name="submit" value="Submit">  
+  </form>
+  </div>
+  </center>
 
-}
-else
-{
-  echo "<script> getLocation(); </script>";
-}
-
-?>
-
-
-<center>
-<div class="jumbotron">
-
-
-
-
-<form name="myform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
-  Select Destination: 
-  <select name="destination_string" value="<?php echo $destination_string;?>">
-  <option value="executive_centre">Executive Centre</option>
-  <option value="macdonalds">Macdonalds</option>
-  <option value="pioneer_hall">Pioneer Hall</option>
-  </select>
-  <br><br>
-  Use Current Location? : 
-  <select name="use_current_location" value="<?php echo $use_current_location;?>">
-  <option value="yes">YES</option>
-  <option value="no">NO</option>
-  </select>
-  <br><br>
-  <input type="hidden" name="cLat" value="<?php echo $cLat;?>" >
-  <input type="hidden" name="cLon" value="<?php echo $cLon;?>" >
-  <input type="submit" name="submit" value="Submit">  
-</form>
-</div>
-</center>
-<!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
